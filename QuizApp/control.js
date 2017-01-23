aafeefc2e1415f66e06dcd9f47091e9460ad9c96
Rahul Rasgon <rahulrasgon@gmail.com>
2017-01-23 11:43:28 +0530

    bugs removed, Variable Naming Convention Followed

--- QuizApp/control.js
+++ QuizApp/control.js
@@ -1,5 +1,5 @@
 //Array Of Questions
-var array = [
+var questionArray = [
   {
     "question":"What does HTML stand for?",
     "option1":"Hyper Text Markup Language",
@@ -29,33 +29,33 @@ var array = [
 //Global Variables
 var index=0;
 var score=0;
-var totalquestion=array.length;
-var quizcontainer = document.getElementById('quizcontainer');
+var totalQuestion=questionArray.length;
+var quizContainer = document.getElementById('quiz-container');
 var questions = document.getElementById('questions');
 var opt1 = document.getElementById('option1');
 var opt2 = document.getElementById('option2');
 var opt3 = document.getElementById('option3');
 var opt4 = document.getElementById('option4');
-var nextbutton = document.getElementById('nextbutton');
-var nextquestion=document.getElementById('nextquestion');
-var outofquestion=document.getElementById('outofquestion');
+var nextButton = document.getElementById('next-button');
+var nextQuestion=document.getElementById('next-question');
+var outOfQuestion=document.getElementById('out-of-question');
 var notification1=document.getElementById('notification');
-var startquiz1=document.getElementById('startquiz1');
-var resetquiz=document.getElementById('resetquiz');
+var startQuiz1=document.getElementById('start-quiz');
+var resetQuiz=document.getElementById('reset-quiz');
 
 //Show Question Function
-function showquestions(index) {
-  var ques=array[index];
+function showQuestions(index) {
+  var ques=questionArray[index];
   questions.innerHTML ="Question "+(index+1)+":-    "+ques.question;
   opt1.innerHTML = ques.option1;
   opt2.innerHTML = ques.option2;
   opt3.innerHTML = ques.option3;
   opt4.innerHTML = ques.option4;
-  outofquestion.innerHTML="question "+(index+1)+" of "+totalquestion;
+  outOfQuestion.innerHTML="question "+(index+1)+" of "+totalquestion;
 }
 
 //Load Next Question Function
-function nextquestion1() {
+function nextQuestion1() {
   var tickans=document.querySelector('input[type=radio]:checked');
   if(!tickans){
     notification1.style.visibility = 'visible';
@@ -63,26 +63,26 @@ function nextquestion1() {
     return;
   }
   tickans.checked=false;
-  if(tickans.value==array[index].answer){
+  if(tickans.value==questionArray[index].answer){
     score++;
   }
   index=index+1;
   notification1.style.visibility = 'hidden';
-  if(index==totalquestion-1) {
-    nextquestion.innerHTML="Finish";
+  if(index==totalQuestion-1) {
+    nextQuestion.innerHTML="Finish";
   }
-  if(index==totalquestion) {
-    quizcontainer.style.display='none';
-    nextbutton.innerHTML="Score is "+score+" Out of "+totalquestion;
-    resetquiz.style.visibility='visible';
+  if(index==totalQuestion) {
+    quizContainer.style.display='none';
+    nextButton.innerHTML="Score is "+score+" Out of "+totalQuestion;
+    resetQuiz.style.visibility='visible';
   }
-  showquestions(index);
+  showQuestions(index);
 }
 
 //Start Question Function
-function startquiz() {
-  startquiz1.style.display='none';
-  nextbutton.style.visibility='visible'
-  quizcontainer.style.visibility='visible';
-  showquestions(index);
+function startQuiz() {
+  startQuiz1.style.display='none';
+  nextButton.style.visibility='visible'
+  quizContainer.style.visibility='visible';
+  showQuestions(index);
 }
